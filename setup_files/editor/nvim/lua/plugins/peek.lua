6433e9f535e7c61baaeee693022ef260d2358c51
Dennis Xu <xudennis8@gmail.com>
2024-05-24 19:19:27 -0500

    Add peek.nvim markdown viewer

---
+++ setup_files/editor/nvim/lua/plugins/peek.lua
@@ -0,0 +1,14 @@
+local plugin = {"toppair/peek.nvim"}
+
+plugin.event = {"VeryLazy"}
+
+plugin.build = "deno task --quiet build:fast"
+
+function plugin.config()
+    require("peek").setup()
+    -- refer to `configuration to change defaults`
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+end
+
+return plugin
