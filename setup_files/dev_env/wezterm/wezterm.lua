0570c6eecfdc9aa94dc97483b820dc6328871936
Dennis Xu <xudennis8@gmail.com>
2024-07-20 14:12:13 -0500

    Update wezterm color scheme
    Update wezterm config:
    - update font size
    - switch to Hack Nerd Font
    - disable brightening colors that are bold
    - update padding

--- setup_files/dev_env/wezterm/wezterm.lua
+++ setup_files/dev_env/wezterm/wezterm.lua
@@ -4,14 +4,23 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
 -- Color scheme
 config.color_scheme = 'Ubuntu'
-config.font = wezterm.font_with_fallback {'SauceCodePro Nerd Font', 'ProFont IIx Nerd Font'}
-config.font_size = 11.5
+config.bold_brightens_ansi_colors = false
+config.font = wezterm.font_with_fallback {'SauceCodePro Nerd Font Mono', 'Hack Nerd Font'}
+config.font_size = 12
+config.freetype_load_target = "Light"
 
 -- Scrollback settings
 config.enable_scroll_bar = true
 config.scrollback_lines = 100000
+
+config.window_padding = {
+    top = "2px",
+    bottom = 0,
+}
 
 local act = wezterm.action
 config.keys = {
