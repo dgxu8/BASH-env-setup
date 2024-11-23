aba8bfeec372993aad3a59943c69fb80d9e437e9
Dennis Xu <xudennis8@gmail.com>
2024-11-22 21:40:10 -0600

    Add tio
    Add tio serial console.

--- setup_files/misc/symlink.sh
+++ setup_files/misc/symlink.sh
@@ -4,3 +4,6 @@ my_path=$1
 
 echo "Symlinking pylint"
 ln -sfv "${my_path}/pylintrc" "${HOME}/.pylintrc"
+
+echo "Symlinking tio config"
+ln -sfv "${my_path}/tio_config" "${HOME}/.config/tio/config"
