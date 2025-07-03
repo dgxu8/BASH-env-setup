c4180a635139d6469e2b5097640e2ed76c5bca4e
Dennis Xu <xudennis8@gmail.com>
2025-07-03 11:17:25 -0500

    Update nvim completion look and add copilot

--- setup_files/editor/nvim/lua/plugins/nvim-cmp.lua
+++ setup_files/editor/nvim/lua/plugins/nvim-cmp.lua
@@ -1,190 +1,192 @@
-local plugin = {"hrsh7th/nvim-cmp"}
+return {
+    {
+        "hrsh7th/nvim-cmp",
 
-plugin.dependencies = {
-    -- Sources
-    {"hrsh7th/cmp-buffer"},
-    {"hrsh7th/cmp-path"},
-    {"saadparwaiz1/cmp_luasnip"},
-    {"hrsh7th/cmp-nvim-lsp"},
+        version = false,
+        event = "insertenter",
 
-    -- cmp icons
-    {"onsails/lspkind-nvim"},
+        dependencies = {
+            -- sources
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
 
-    -- Snippets
-    {"L3MON4D3/LuaSnip"},
-    {"rafamadriz/friendly-snippets"},
+            -- cmp icons
+            "onsails/lspkind-nvim",
 
-    -- Completion scores
-    {"p00f/clangd_extensions.nvim"}
-}
+            -- snippets
+            "l3mon4d3/luasnip",
+            "rafamadriz/friendly-snippets",
 
-plugin.event = "InsertEnter"
-
-function plugin.config()
-    vim.opt.completeopt = {"menu", "menuone", "noselect"}
-
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-
-    require("luasnip.loaders.from_vscode").lazy_load()
-
-    -- Customization for Pmenu
-    vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#282C34", fg = "NONE" })
-    vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#22252A" })
-
-    vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
-    vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
-    vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
-    vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
-
-    vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
-    vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
-    vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
-
-    vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
-    vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
-    vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
-
-    vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
-    vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
-    vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
-
-    vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
-    vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
-    vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
-    vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
-    vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
-
-    vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
-    vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
-
-    vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
-    vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
-    vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
-
-    vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
-    vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
-    vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
-
-    vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
-    vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
-    vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
-
-    local has_words_before = function()
-        if vim.api.nvim_get_option_value("buftype", {}) == "prompt" then return false end
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-    end
-
-    local select_opts = {behavior = cmp.SelectBehavior.Select}
-    cmp.setup({
-        formatting = {
-            fields = { "kind", "abbr", "menu" },
-            format = function(entry, vim_item)
-                local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 60 })(entry, vim_item)
-                local strings = vim.split(kind.kind, "%s", { trimempty = true })
-                kind.kind = " " .. (strings[1] or "") .. " "
-                kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-                return kind
-            end,
+            -- completion scores
+            "p00f/clangd_extensions.nvim",
         },
-        window = {
-            completion = {
-                winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-                col_offset = -3,
-                side_padding = 0,
-            },
-        },
-        snippet = {
-            expand = function(args)
-                luasnip.lsp_expand(args.body)
+
+        opts = function ()
+            -- vim.opt.completeopt = {"menu", "menuone", "noselect"}
+
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            local defaults = require("cmp.config.default")()
+            local auto_select = false
+
+            -- require("luasnip.loaders.from_vscode").lazy_load()
+
+            vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
+            -- customization for pmenu
+            vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#454952", fg = "NONE" })
+            vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#31353f" })
+
+            vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+            vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+            vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+            vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+
+            vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
+            vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
+            vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
+
+            vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
+            vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
+            vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
+
+            vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
+            vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
+            vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
+
+            vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
+            vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
+            vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
+            vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
+            vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
+
+            vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
+            vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
+
+            vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
+            vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
+            vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
+
+            vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
+            vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
+            vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
+
+            vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
+            vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
+            vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
+
+            vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#e4f0df", bg ="#569e36" })
+
+            local has_words_before = function()
+                if vim.api.nvim_get_option_value("buftype", {}) == "prompt" then return false end
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
             end
-        },
-        sorting = {
-            priority_weight = 2,
-            comparators = {
-                cmp.config.compare.offset,
-                cmp.config.compare.exact,
-                cmp.config.compare.recently_used,
-                require("clangd_extensions.cmp_scores"),
-                cmp.config.compare.kind,
-                cmp.config.compare.sort_text,
-                cmp.config.compare.length,
-                cmp.config.compare.order,
-            },
-        },
-        sources = {
-            {name = "path"},
-            {name = "nvim_lsp"},
-            {name = "buffer", keyword_length = 3},
-            {name = "luasnip", keyword_length = 2},
-        },
-        mapping = {
-            ["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
-            ["<C-n>"] = cmp.mapping.select_next_item(select_opts),
-            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-e>"] = cmp.mapping.abort(),
-            ["<cr>"] = cmp.mapping.confirm {
-                behavior = cmp.ConfirmBehavior.Insert,
-                select = true,
-            },
-            ["<Tab>"] = vim.schedule_wrap(function(fallback)
-                if cmp.visible() and has_words_before() then
-                    cmp.select_next_item(select_opts)
-                elseif luasnip.expand_or_jumpable() then
-                    luasnip.expand_or_jump()
-                else
-                    fallback()
-                end
-            end),
-            ["<S-Tab>"] = vim.schedule_wrap(function(fallback)
-                if cmp.visible() and has_words_before() then
-                    cmp.select_prev_item(select_opts)
-                elseif luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
-                else
-                    fallback()
-                end
-            end),
-        },
-    })
 
-    local bufIsBig = function(bufnr)
-        local max_filesize = 200 * 1024 -- 200 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        else
-            return false
-        end
-    end
+            return {
+                auto_brackets = {},
+                completion = {
+                    completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+                },
+                preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+                mapping = {
+                    ["<c-p>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}),
+                    ["<c-n>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}),
+                    ["<c-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<c-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<c-Space>"] = cmp.mapping.complete(),
+                    ["<c-e>"] = cmp.mapping.abort(),
+                    ["<cr>"] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    },
+                    ["<tab>"] = vim.schedule_wrap(function(fallback)
+                        if cmp.visible() and has_words_before() then
+                            cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end),
+                    ["<S-tab>"] = vim.schedule_wrap(function(fallback)
+                        if cmp.visible() and has_words_before() then
+                            cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end),
+                },
+                sources = cmp.config.sources ({
+                    {name = "nvim_lsp"},
+                    {name = "path"},
+                }, {
+                    {name = "buffer", keyword_length = 3},
+                    {name = "luasnip", keyword_length = 2},
+                }),
+                formatting = {
+                    fields = { "kind", "abbr", "menu" },
+                    format = function(entry, item)
+                        local icons = require("lspkind").cmp_format({
+                            mode = "symbol_text", maxwith = 100, symbol_map = { Copilot = ""}
+                        })(entry, item)
 
-    -- default sources for all buffers
-    local default_cmp_sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lsp_signature_help' },
-    }, {
-        { name = 'vsnip' },
-        { name = 'path' }
-    })
+                        -- splits the icon and menu strings. Icon will be in the front and menu will be in the back
+                        local strings = vim.split(icons.kind, "%s", { trimempty = true })
+                        icons.kind = " " .. (strings[1] or "") .. " "
+                        icons.menu = "    (" .. (strings[2] or "") .. ")"
 
-    -- If a file is too large, I don't want to add to it's cmp sources treesitter, see:
-    -- https://github.com/hrsh7th/nvim-cmp/issues/1522
-    vim.api.nvim_create_autocmd('BufReadPre', {
-        callback = function(t)
-            local sources = default_cmp_sources
-            if not bufIsBig(t.buf) then
-                sources[#sources+1] = {name = 'treesitter', group_index = 2}
-            end
-            cmp.setup.buffer {
-                sources = sources
+                        -- lazyvim junk to make the completion menu look nice
+                        if icons[item.kind] then
+                            item.kind = icons[item.kind] .. item.kind
+                        end
+
+                        local widths = {
+                            abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 70,
+                            menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 70,
+                        }
+
+                        for key, width in pairs(widths) do
+                            if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+                            end
+                        end
+
+                        return item
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered({
+                        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:Visual,Search:None",
+                        col_offset = -3,
+                        side_padding = 0,
+                    }),
+                    documentation = cmp.config.window.bordered(),
+                },
+                experimental = {
+                    ghost_text = {
+                        hl_group = "CmpGhostText",
+                    },
+                },
+                sorting = {
+                    priority_weight = 2,
+                    comparators = {
+                        cmp.config.compare.offset,
+                        cmp.config.compare.exact,
+                        cmp.config.compare.recently_used,
+                        require("clangd_extensions.cmp_scores"),
+                        cmp.config.compare.locality,
+                        cmp.config.compare.kind,
+                        cmp.config.compare.sort_text,
+                        cmp.config.compare.length,
+                        cmp.config.compare.order,
+                    },
+                },
             }
-        end
-    })
-
-end
-
-return plugin
+        end,
+    },
+}
